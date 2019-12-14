885335c365e5b4a3a8407b93ba9fd3d298c171a9
MonotoneBijections <47801570+MonotoneBijections@users.noreply.github.com>
2019-12-14 21:30:34 +0800

    Add files via upload

---
+++ HW6/todo.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def svd_compress(imArr, K=50):
+    """Compress image array using SVD decomposition.
+    Arg:
+        imArr: numpy array with shape (height, width, 3).
+    Return:
+        Compressed imArr: numpy array.
+    """
+    imArr_compressed = np.zeros(imArr.shape)
+    # For each channel
+    for ch in range(3):
+        # --------------------
+        # TODO: 
+        #     Compress the image array using SVD decomposition
+        # hint:
+        #     1. numpy.linalg.svd
+        #     2. numpy.diag
+        #     3. numpy.dot
+        #
+        # Your code here
+        # imArr_compressed = ??
+        # 
+        # --------------------
+        # print(imArr[:,:,ch])
+        u,s,v = np.linalg.svd(imArr[:,:,ch],full_matrices= False) 
+        # get the 2d
+        s_diag =  np.diag(s)  
+        print(np.diag(s))
+        # return the diagonal value in the matrix
+        s[K:] = 0.0
+        # mask out the other elements 
+        imArr_compressed[:,:,ch] =np.dot(np.dot (u , s) , v)
+     
+        
+            
+            
+        
+        
+        
+        # Make imArr_compressed range from 0 to 255
+        imArr_compressed[:, :, ch] -= imArr_compressed[:, :, ch].min()
+        imArr_compressed[:, :, ch] /= imArr_compressed[:, :, ch].max()
+        imArr_compressed[:, :, ch] *= 255
+        # Return uint8 because save_image needs input of type uint8
+    return imArr_compressed.astype(np.uint8)
